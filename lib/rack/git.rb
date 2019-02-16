8b053e653b9414b94e7951f746b52626272b14a1
Naoto Kaneko <naoty.k@gmail.com>
2019-02-16 16:31:55 +0900

    Enable to use Rack::Git as middleware

--- lib/rack/git.rb
+++ lib/rack/git.rb
@@ -1,7 +1,15 @@
 module Rack
   class Git
+    def initialize(app = nil)
+      @app = app
+    end
+
     def call(env)
-      [200, { "Content-Type" => "text/plain" }, [commit]]
+      return [200, { "Content-Type" => "text/plain" }, [commit]] if @app.nil?
+
+      response = @app.call(env)
+      response[1]["X-Git-Commit"] = commit
+      response
     end
 
     private
